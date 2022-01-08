d9e2b7a0e0e9c659c785d44fa3fe7c895627cae1
dm1itri <dimma-18@yandex.ru>
2022-01-08 11:35:16 +0300

    add dialogs menu

--- menu.py
+++ menu.py
@@ -16,18 +16,6 @@ def terminate():
     sys.exit()
 
 
-class SimpleDialog(gui.Dialog):
-    def __init__(self):
-        title = gui.Label("Spam")
-        main = gui.Container(width=20, height=20)
-        # I patched PGU to use new style classes.
-        super(SimpleDialog, self).__init__(title, main, width=40, height=40)
-
-    def close(self, *args, **kwargs):
-        print("closing")
-        return super(SimpleDialog, self).close(*args, **kwargs)
-
-
 class Menu:
     def __init__(self):
         self.main_font = 'additional_functions/fonts/main.ttf'
